9b397e8395c35ba51e83d07500930f5931c5ca05
bvenkateswarlu <venki.manikanta@gmail.com>
2020-11-09 22:24:03 +0000

    Fixed issue while processing apdu

--- src/JCardSimulator.java
+++ src/JCardSimulator.java
@@ -153,7 +153,7 @@ public class JCardSimulator implements Simulator {
 	}
 	
 	private byte[] processApdu(byte[] apdu) {
-		if(apdu.length > 256) {
+		if(apdu[4] == 0x00 && apdu.length > 256) {
 			byte[] returnApdu = new byte[apdu.length-3];
 			for(int i = 0; i < returnApdu.length; i++)
 				returnApdu[i] = apdu[i];
